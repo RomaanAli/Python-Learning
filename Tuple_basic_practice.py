9500569524c64b9f719508e3809fc45f93e74a8a
RomaanAli <mr.romaanali@gmail.com>
2026-07-24 11:24:42 +0500

    tuple practiced by for and while loop

---
+++ Tuple_basic_practice.py
@@ -0,0 +1,11 @@
+#loop practice in tuple
+subjects = ("Math", "Physics", "English", "Programming")
+
+for subject in subjects:
+    print(subject)
+
+i=0
+while i < len(subjects):
+    print(subjects[i])
+    i+=1
+
