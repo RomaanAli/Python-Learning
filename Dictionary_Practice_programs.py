aa6fd96a35ff13b2b9e5bd1662d613082ee31386
RomaanAli <mr.romaanali@gmail.com>
2026-07-27 10:33:26 +0500

    practe keys,values,items in loop

--- Dictionary_Practice_programs.py
+++ Dictionary_Practice_programs.py
@@ -12,11 +12,10 @@ def display():
     for key,value in student.items():
         print(f"{key}: {value}")
 
-
+"""
 #removing values
 print("deleting value with --id-- pop():",student.pop("marks"))
 display()
-
 print("deleting value with popitem():",student.popitem())
 display()
 print("deleting value with - del():")
@@ -24,3 +23,14 @@ del student["roll_no"]
 display()
 print("deleting value with --id-- clear():",student.clear())
 display()
+"""
+#applying loops on dictionary
+print("For loop with .items both key and value.")
+for key,value in student.items():
+    print(f"{key}: {value}")
+#for loop with just keys
+for key in student.keys():
+    print(f"{key}")
+#for loop with values:
+for value in student.values():
+        print(f"{value}:")
