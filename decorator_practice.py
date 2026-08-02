c4973a84532bcfbd248e5435e3d79c605385adfe
RomaanAli <mr.romaanali@gmail.com>
2026-08-02 21:53:55 +0500

    Non-local data practice

--- decorator_practice.py
+++ decorator_practice.py
@@ -9,3 +9,18 @@ def outer():
 greet = outer()
 
 greet()
+
+
+def multiply_by(n):
+
+    def multiply(x):
+        return x * n
+
+    return multiply
+
+double = multiply_by(2)
+
+triple = multiply_by(3)
+
+print(double(5))
+print(triple(5))
