1f9065976dbd011de58aee1ed1454fd742808855
RomaanAli <mr.romaanali@gmail.com>
2026-08-06 09:27:44 +0500

    finding missing value till n

--- list_practice_prolems.py
+++ list_practice_prolems.py
@@ -1,3 +1,8 @@
+li = []
+def list_get():
+    for i in range(int(input("Enter how many numbers you want to enter:"))):
+        li.append(int(input("Enter a number:")))
+    print(li)
 """li=[10,20,30,40,50]
 avg=sum(li)/len(li)
 print(int(avg))
@@ -18,7 +23,7 @@ for i in li:
     if i==n:
         no+=1
 print(f"The ---{n}---  appears {no} times")
-"""
+
 #li=[10,20,30,40,50]
 li=[]
 for i in range(int(input("Enter how many numbers you want to enter:"))):
@@ -33,3 +38,18 @@ for i,num in enumerate(li):
 
 print(f"the first occurrence is {ind[0]} and the last occurrence is {ind[-1]}")
 print(f"These are all index where {f} occurs:",ind)
+
+"""
+new=[]
+missing=[]
+list_get()
+to_n=int(input("Enter n number to make a list:"))
+for i in range(to_n+1):
+    new.append(i)
+for i in new:
+    if i not in li:
+        missing.append(i)
+missing.sort()
+print("missing values are:",missing)
+
+
